c07f7dadbdada3dc40cc6cbdcd496f7eb0cf71c7
Aleksey Gladyshuk <106761534+StarKingdom-Front@users.noreply.github.com>
2023-02-05 17:55:09 +0300

    feat: ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+const one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const ten = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const twenty = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const hundred = 'hundred';
+
 module.exports = function toReadable (number) {
-  
+
+  num = number.toString();
+
+  if (num == 0){
+    result = 'zero';
+    return result;
+  } else if (num < 10) {
+    result = `${one[num]}`;
+    return result.trim();
+  } else if (num >= 10 && num < 20) {
+    result = `${ten[str[1]]}`;
+    return result.trim();
+  } else if (num >= 20 && num < 100) {
+    result = `${twenty[num[0]]} ${one[num[1]]}`;
+    return result.trim();
+  } else if (num >= 100 && num[1] != 1 && num[1] != 0){
+    result = `${one[num[0]]} ${hundred} ${twenty[num[1]]} ${one[num[2]]}`;
+    return result.trim();
+  } else if (num >= 100 && num[1] == 1 && num[1] != 0) {
+    result = `${one[num[0]]} ${hundred} ${ten[num[2]]}`;
+    return result.trim();
+  } else if (num >= 100 && num[1] == 0) {
+    result = `${one[num[0]]} ${hundred} ${one[num[2]]}`;
+    return result.trim();
+  } else {
+    return result;
+  }
 }
